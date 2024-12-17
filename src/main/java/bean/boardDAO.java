a5afda7ad59702062cc0a1f504a003d86ad19aa1
yeong <yeonghak2207@gmail.com>
2024-12-17 10:50:06 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/bean/MemberDAO.java
    #	src/main/webapp/JSP3/MemberList.jsp

--- src/main/java/bean/boardDAO.java
+++ src/main/java/bean/boardDAO.java
@@ -8,9 +8,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class boardDAO {
+
+
     Connection con;
-    PreparedStatement pst;
+    PreparedStatement pstmt;
     ResultSet rs;
+
+
+
     //데이터베이스에 커넥션 Pool을 사용 메서드
     public void getCon(){
         try {
@@ -31,6 +36,66 @@ public class boardDAO {
 
         }catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    //받은 bean 객체를 데이터에 저장하는 메서드, 저장만 하면 되기 때문에 리턴값은 void로 처리한다.
+    public void insertBoard(boardBean bean){
+        getCon();
+        //BoardWriteForm.jsp에서 BoardWriteProc 작성자,제목,이메일,비밀번호,내용을 보내는데 이를 제외한 나머지는 초기화해야한다
+
+        //ref(글 그룹)쿼리를 실행시켜 가장 큰 ref에 +1을 한다., 그래서 첫 글은 그룹이 1일 테니, 나중에 1을 더할거라 지금은 0으로 초기화
+        int ref=0;
+
+        // 새 글이기에 step과 level을 1로 초기화
+        // re_step: 답글의 순서를 나타냄. 원글은 1로 시작
+        int re_step=1;
+        // re_level: 답글의 깊이를 나타냄. 원글은 1로 시작
+        int re_level=1;
+
+
+        try {
+
+            // 가장 큰 ref 값을 가져오는 SQL 쿼리
+            String refsql = "SELECT MAX(ref) from BOARD";
+
+            //쿼리 준비
+            pstmt = con.prepareStatement(refsql);
+            //쿼리 실행 후 결과값을 리턴
+            rs = pstmt.executeQuery();
+            if(rs.next()){//쿼리 실행했을 때의 결과값이 있다면?
+                ref=rs.getInt(1) + 1; // 현재 ref의 최대값에 +1을 더하여 새로운 글 그룹 값으로 설정
+            }
+
+
+            // 매개변수로 받은 bean 객체를 DB에 저장하는 SQL 쿼리
+            // board_seq.nextval: 시퀀스를 사용하여 자동으로 증가하게 함
+            // sysdate: 현재 날짜와 시간을 자동으로 입력
+            // ReadCount는 0으로 초기화 (새 글이므로 아직 조회되지 않음)
+            String sql = "insert into board values(board_seq.nextval,?,?,?,?,sysdate,?,?,?,0,?)";
+            pstmt = con.prepareStatement(sql);
+
+            // ?에 넣을 값을 맵핑
+            pstmt.setString(1, bean.getWriter());
+            pstmt.setString(2, bean.getEmail());
+            pstmt.setString(3, bean.getSubject());
+            pstmt.setString(4, bean.getPassword());
+            pstmt.setInt(5, ref);
+            pstmt.setInt(6, re_step);
+            pstmt.setInt(7, re_level);
+            pstmt.setString(8, bean.getContent());
+
+            //쿼리 실행
+            pstmt.executeUpdate();
+
+            //자원 반납
+            con.close();
+
+
+        }catch (Exception e) {
+            System.err.println("DB에 저장하지 못 하였습니다.");
+            e.printStackTrace();
+            return;
         }
     }
 }
--- src/main/webapp/JSP6/BoardWriteProc.jsp
+++ src/main/webapp/JSP6/BoardWriteProc.jsp
@@ -1,4 +1,4 @@
-<%--
+<%@ page import="bean.boardDAO" %><%--
   Created by IntelliJ IDEA.
   User: yeong
   Date: 2024-12-16
@@ -12,5 +12,27 @@
 </head>
 <body>
 
+<%
+        request.setCharacterEncoding("UTF-8");
+%>
+
+<%--작성한 게시글을 한 번에 읽는다.--%>
+
+<jsp:useBean id="boardbean" class="bean.boardBean">
+<%--BoardWriteForm.jsp에서 보내는 파라미터들을 1:1로 매핑시키는데 안 보내는 파라미터는 자동으로 Null로 매핑--%>
+    <jsp:setProperty name="boardbean" property="*"/>
+</jsp:useBean>
+
+<%--데이터베이스쪽으로 1:1 매핑된 Bean 객체를 넘겨주어 처리한다. 이때 DAO(Coonection Pool)을 사용한다--%>
+<%
+    //DAO(Connection Pool) 객체 생성
+    boardDAO bdao = new boardDAO();
+    //데이터베이스 저장 메서드 호출하는데 bean 객체를 넣어준다.
+    bdao.insertBoard(boardbean);
+
+%>
 </body>
 </html>
+
+
+
